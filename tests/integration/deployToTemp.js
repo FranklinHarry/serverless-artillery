d7365440322a4216923ccfe5f6c0cc7d6c41ee17
Charles Sullivan <charles.sullivan@nordstrom.com>
2018-10-31 17:24:00 +0000

    split filesystem-specific functionality into its own module

--- tests/integration/deployToTemp.js
+++ tests/integration/deployToTemp.js
@@ -1,42 +1,15 @@
 const { tmpdir } = require('os')
-const {
-  join,
-  basename,
-  sep,
-  isAbsolute,
-} = require('path')
+const { join } = require('path')
 const childProcess = require('child_process')
-const fs = require('fs')
 
 const { randomString } = require('./target/handler')
+const fs = require('./fs')
 
 const defaultSourcePath = join(__dirname, 'target')
 const defaultRoot = join(tmpdir(), 'slsart-integration')
 
-const flatten = values =>
-  values.reduce((flattened, value) =>
-    (Array.isArray(value)
-      ? flattened.concat(flatten(value))
-      : flattened.concat([value])), [])
-
-const joinFirstTwo = parts =>
-  [join(parts[0] || sep, parts[1]), ...parts.slice(2)]
-
-const splitPath = path =>
-  (isAbsolute(path)
-    ? joinFirstTwo(path.split(sep))
-    : path.split(sep))
-
-const joinToLast = (list, value) =>
-  (list.length
-    ? join(list[list.length - 1], value)
-    : value)
-
-const progressivePaths = path =>
-  splitPath(path).reduce(
-    (list, part) => [...list, joinToLast(list, part)],
-    []
-  )
+const execError = (err, stderr) =>
+  new Error(`${err.message} ${stderr}`)
 
 const namesToFullPaths = directory =>
   names =>
@@ -45,69 +18,20 @@ const namesToFullPaths = directory =>
 const filterOutSpecFiles = names =>
   names.filter(name => !name.endsWith('.spec.js'))
 
-const sourceFileNameTo = destination =>
-  sourceFileName =>
-    join(destination, basename(sourceFileName))
-
-const execError = (err, stderr) =>
-  new Error(`${err.message} ${stderr}`)
-
 const pure = {
-  mkdir: (mkdir = fs.mkdir) =>
-    directory =>
-      new Promise(resolve => mkdir(directory, resolve)),
-
-  mkdirp: (mkdir = pure.mkdir) =>
-    directory => progressivePaths(directory)
-      .reduce(
-        (awaiting, nextPath) => awaiting.then(() => mkdir(nextPath)),
-        Promise.resolve()
-      ),
-
-  ls: (readdir = fs.readdir) =>
-    directory =>
-      new Promise(resolve =>
-        readdir(directory, (ignoredError, names) =>
-          resolve(names || []))),
-
-  cp: (copyFile = fs.copyFile) =>
-    destination =>
-      source =>
-        new Promise(resolve => copyFile(source, destination, resolve)),
-
-  findTargetSourceFiles: (ls = pure.ls(), sourcePath = defaultSourcePath) =>
+  findTargetSourceFiles: (ls = fs.ls, sourcePath = defaultSourcePath) =>
     () =>
       ls(sourcePath)
         .then(filterOutSpecFiles)
         .then(namesToFullPaths(sourcePath)),
 
-  copyTofolder: (cp = pure.cp()) =>
-    (destination) => {
-      const toDestination = sourceFileNameTo(destination)
-      return filePath =>
-        cp(toDestination(filePath))(filePath)
-    },
-
-  copyAll: (copyTofolder = pure.copyTofolder()) =>
-    destination =>
-      sourceFiles =>
-        Promise.all(sourceFiles.map(copyTofolder(destination))),
-
-  writeFile: (writeFile = fs.writeFile) =>
-    (destination, data) =>
-      new Promise((resolve, reject) =>
-        writeFile(destination, data, err =>
-          (err
-            ? reject(err)
-            : resolve()))),
-
-  writeConfig: (writeFile = pure.writeFile()) =>
+  writeConfig: (writeFile = fs.writeFile) =>
     (destination, instanceId) =>
       writeFile(join(destination, 'config.yml'), `instanceId: ${instanceId}`),
 
   stageTarget: (
     findTargetSourceFiles = pure.findTargetSourceFiles(),
-    copyAll = pure.copyAll(),
+    copyAll = fs.copyAll,
     writeConfig = pure.writeConfig()
   ) =>
     (destination, instanceId) =>
@@ -133,7 +57,7 @@ const pure = {
 
   deployNewTarget: (
     tempLocation = pure.tempLocation(),
-    mkdirp = pure.mkdirp(),
+    mkdirp = fs.mkdirp,
     stageTarget = pure.stageTarget(),
     deploy = pure.deploy(),
     log = console.log,
@@ -154,43 +78,11 @@ const pure = {
     directory =>
       exec('sls remove', { cwd: directory }),
 
-  listAbsolutePathsRecursively: (ls = pure.ls()) => {
-    const listNext = directory =>
-      ls(directory)
-        .then(files => files.map(file => join(directory, file)))
-        .then(files => Promise.all(files.map(listNext)))
-        .then(children => [...flatten(children), directory])
-    return listNext
-  },
-
-  rm: (unlink = fs.unlink) =>
-    path =>
-      new Promise(resolve =>
-        unlink(path, resolve)),
-
-  rmdir: (rmdir = fs.rmdir) =>
-    path =>
-      new Promise(resolve =>
-        rmdir(path, resolve)),
-
-  rmAny: (rm = pure.rm(), rmdir = pure.rmdir()) =>
-    path =>
-      rm(path).then(err => (err ? rmdir(path) : undefined)),
-
-  rmrf: (
-    listAll = pure.listAbsolutePathsRecursively(),
-    rm = pure.rmAny()
-  ) =>
-    directory =>
-      listAll(directory)
-        .then(files => [...files, directory])
-        .then(files => Promise.all(files.map(rm))),
-
   removeTempDeployment: (
     log = console.log,
     remove = pure.remove(),
     warn = console.error,
-    rmrf = pure.rmrf()
+    rmrf = fs.rmrf
   ) =>
     directory =>
       log('removing temp deployment', directory) || remove(directory)
@@ -198,7 +90,7 @@ const pure = {
         .then(() => log('deleting', directory) || rmrf(directory))
         .then(() => log('done')),
 
-  listTempDeployments: (ls = pure.ls()) =>
+  listTempDeployments: (ls = fs.ls) =>
     root =>
       ls(root)
         .then(directories =>
--- tests/integration/deployToTemp.spec.js
+++ tests/integration/deployToTemp.spec.js
@@ -4,18 +4,10 @@ const {
   },
 } = require('chai')
 const { sep } = require('path')
-const os = require('os')
 
 const {
   pure: {
-    mkdir,
-    mkdirp,
-    ls,
-    cp,
     findTargetSourceFiles,
-    copyTofolder,
-    copyAll,
-    writeFile,
     writeConfig,
     stageTarget,
     execAsync,
@@ -23,11 +15,6 @@ const {
     tempLocation,
     deployNewTarget,
     remove,
-    listAbsolutePathsRecursively,
-    rm,
-    rmdir,
-    rmAny,
-    rmrf,
     removeTempDeployment,
     listTempDeployments,
     cleanupDeployments,
@@ -93,71 +80,6 @@ const sequence = (functions) => {
 }
 
 describe('./tests/integration/deployToTemp', () => {
-  describe('#mkdir', () => {
-    const mkdirOk = mockback([values.directory])
-    const mkdirFail =
-      mockback([values.directory], values.err)
-    it('should resolve empty on success', () =>
-      mkdir(mkdirOk)(values.directory)
-        .then(missing))
-    it('should resolve with the error on fail', () =>
-      mkdir(mkdirFail)(values.directory)
-        .then(err => deepStrictEqual(err, values.err)))
-  })
-
-  describe('#mkdirp', () => {
-    const rootPath = os.type() === 'Windows_NT'
-      ? `c:${sep}`
-      : sep
-    const mkdirpWithMockSequence = (directory, mockSequence) => {
-      const mocks = sequence(mockSequence)
-      return mkdirp(mocks.mkdir)(directory)
-    }
-    it('should create progressive paths and resolve', () =>
-      mkdirpWithMockSequence(
-        `${rootPath}foo${sep}bar`,
-        [
-          ['mkdir', [`${rootPath}foo`], Promise.resolve()],
-          ['mkdir', [`${rootPath}foo${sep}bar`], Promise.resolve()],
-        ]).then(missing))
-    it('should resolve despite errors', () =>
-      mkdirpWithMockSequence(
-        `${rootPath}foo${sep}bar`,
-        [
-          ['mkdir', [`${rootPath}foo`], Promise.resolve(values.err)],
-          ['mkdir', [`${rootPath}foo${sep}bar`], Promise.resolve()],
-        ]).then(missing))
-    it('should resolve the final error', () =>
-      mkdirpWithMockSequence(
-        `${rootPath}foo${sep}bar`,
-        [
-          ['mkdir', [`${rootPath}foo`], Promise.resolve()],
-          ['mkdir', [`${rootPath}foo${sep}bar`], Promise.resolve(values.err)],
-        ]).then(isValue('err')))
-  })
-
-  describe('#ls', () => {
-    const readdirOk = mockback([values.directory], undefined, values.names)
-    const readdirFail = mockback([values.directory], values.err)
-    it('should resolve names on success', () =>
-      ls(readdirOk)(values.directory).then(isValue('names')))
-    it('should resolve empty array on fail', () =>
-      ls(readdirFail)(values.directory)
-        .then(names => deepStrictEqual(names, [])))
-  })
-
-  describe('#cp', () => {
-    const copyFileOk = mockback([values.source, values.destination])
-    const copyFileFail =
-      mockback([values.source, values.destination], values.err)
-    it('should resolve empty on success', () =>
-      cp(copyFileOk)(values.destination)(values.source)
-        .then(missing))
-    it('should resolve with the error on fail', () =>
-      cp(copyFileFail)(values.destination)(values.source)
-        .then(err => deepStrictEqual(err, values.err)))
-  })
-
   describe('#findTargetSourceFiles', () => {
     const sourcePath = 'foo'
     const names = ['bar.js', 'bar.spec.js']
@@ -166,45 +88,6 @@ describe('./tests/integration/deployToTemp', () => {
     it('should resolve to target source file full paths', () =>
       findTargetSourceFiles(lsOk, sourcePath)()
         .then(fullPaths => deepStrictEqual(fullPaths, expected)))
-  })
-
-  describe('#copyTofolder', () => {
-    const filePath = `foo${sep}first.js`
-    const destination = 'bar'
-    const expectedCpArgs = [`bar${sep}first.js`, filePath]
-    const cpOk = destinationFile =>
-      sourceFile =>
-        deepStrictEqual([destinationFile, sourceFile], expectedCpArgs) ||
-          Promise.resolve()
-    it('should copy a source file to the destination folder', () =>
-      copyTofolder(cpOk)(destination)(filePath)
-        .then(missing)
-    )
-  })
-
-  describe('#copyAll', () => {
-    const destination = 'bar'
-    const sourceFiles = [`foo${sep}first.js`, `foo${sep}second.js`]
-    const expectedSourceFiles = [...sourceFiles]
-    const copyTofolderOk = directory =>
-      strictEqual(directory, destination) || (actual =>
-        strictEqual(actual, expectedSourceFiles.shift()) || Promise.resolve())
-    it('should copy all source files to the destination directory', () =>
-      copyAll(copyTofolderOk)(destination)(sourceFiles)
-        .then(() => strictEqual(expectedSourceFiles.length, 0))
-        .then(missing))
-  })
-
-  describe('#writeFile', () => {
-    const writeFileArgs = [values.destination, values.data]
-    const writeFileOk = mockback(writeFileArgs)
-    const writeFileFail = mockback(writeFileArgs, values.err)
-    it('should resolve empty on success', () =>
-      writeFile(writeFileOk)(values.destination, values.data)
-        .then(missing))
-    it('should reject with the error on fail', () =>
-      writeFile(writeFileFail)(values.destination, values.data)
-        .then(() => fail('should reject'), isValue('err')))
   })
 
   describe('#writeConfig', () => {
@@ -362,97 +245,6 @@ describe('./tests/integration/deployToTemp', () => {
     it('should pass through exec rejection', () =>
       remove(execAsyncFail)()
         .then(() => fail('should reject'), err => strictEqual(err, error)))
-  })
-
-  describe('#listAbsolutePathsRecursively', () => {
-    const mockLs = fileSystem =>
-      (basePath) => {
-        const dir = basePath.split(sep).reduce(
-          (fileSystemPart, pathPart) => fileSystemPart[pathPart],
-          fileSystem
-        )
-        return Promise.resolve(Object.keys(dir))
-      }
-    const rootPath = 'foo'
-    const assertPathsListedRecursively = (fileSystem, expected) => {
-      const rootedFileSystem = { [rootPath]: fileSystem }
-      return listAbsolutePathsRecursively(mockLs(rootedFileSystem))(rootPath)
-        .then(paths => [...paths].sort())
-        .then(deepStrictEqualTo([...expected].sort()))
-    }
-    it('resolves to only the root path when no sub-files exist', () =>
-      assertPathsListedRecursively({}, [rootPath]))
-    it('resolves to the root path and nested child paths', () =>
-      assertPathsListedRecursively(
-        {
-          l1a: {
-            l2a: {},
-            l2b: {},
-          },
-          l2a: {},
-        },
-        [
-          rootPath,
-          [rootPath, 'l1a'].join(sep),
-          [rootPath, 'l1a', 'l2a'].join(sep),
-          [rootPath, 'l1a', 'l2b'].join(sep),
-          [rootPath, 'l2a'].join(sep),
-        ]
-      ))
-  })
-
-  describe('#rm', () => {
-    const unlinkOk = mockback([values.directory])
-    const unlinkFail =
-      mockback([values.directory], values.err)
-    it('should resolve empty on success', () =>
-      rm(unlinkOk)(values.directory)
-        .then(missing))
-    it('should resolve with the error on fail', () =>
-      rm(unlinkFail)(values.directory)
-        .then(err => deepStrictEqual(err, values.err)))
-  })
-
-  describe('#rmdir', () => {
-    const rmdirOk = mockback([values.directory])
-    const rmdirFail =
-      mockback([values.directory], values.err)
-    it('should resolve empty on success', () =>
-      rmdir(rmdirOk)(values.directory)
-        .then(missing))
-    it('should resolve with the error on fail', () =>
-      rmdir(rmdirFail)(values.directory)
-        .then(err => deepStrictEqual(err, values.err)))
-  })
-
-  describe('#rmAny', () => {
-    const givenDirectoryResolve = returnValue =>
-      arg =>
-        strictEqual(arg, values.directory) || Promise.resolve(returnValue)
-    const rmOk = givenDirectoryResolve(false)
-    const rmdirOk = givenDirectoryResolve(false)
-    const rmFail = givenDirectoryResolve(values.err)
-    const rmdirFail = givenDirectoryResolve(values.err)
-    it('should resolve removing file', () =>
-      rmAny(rmOk, rmdirFail)(values.directory)
-        .then(value => value === false))
-    it('should resolve removing directory', () =>
-      rmAny(rmFail, rmdirOk)(values.directory)
-        .then(value => value === false))
-    it('should resolve to error when remove file and directory fail', () =>
-      rmAny(rmFail, rmdirFail)(values.directory)
-        .then(isValue('err')))
-  })
-
-  describe('#rmrf', () => {
-    const files = ['foo', 'bar']
-    const listAllOk = directory =>
-      isValue('directory')(directory) || Promise.resolve(files)
-    const rmAnyOk = path => Promise.resolve(path)
-    it('should remove all listed files and directories', () =>
-      rmrf(listAllOk, rmAnyOk)(values.directory)
-        .then(removed => [...removed].sort())
-        .then(deepStrictEqualTo([...files, values.directory].sort())))
   })
 
   describe('#removeTempDeployment', () => {
